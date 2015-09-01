f1b5d9d80041138361c8b546c12cc37f96850167
jamlee <1575103047@qq.com>
2015-09-01 12:32:33 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 tty -s
+sudo docker-compose -f yml/datatest.yml up
 sudo docker-compose  -f yml/docker-compose.test.yml up -d \
   && sudo docker-compose  -f yml/docker-compose.test.yml start \
   && sudo docker exec -i  yml_webtest_1  /bin/sh -c 'vendor/bin/phing' \
   && sudo docker-compose  -f yml/docker-compose.test.yml stop \
   && sudo docker-compose  -f yml/docker-compose.test.yml rm -f
+sudo docker-compose -f yml/datatest.yml rm -f
